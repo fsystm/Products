60e66c5f00ba4c26c66290dd3f16552fb864176a
Fsystm <emirhan25.2003@hotmail.com>
2020-05-19 04:00:58 +0300

    Updates

---
+++ precache-manifest.da05a092289888a258801c74c06fcbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906ebb699e50fbd12bbe7a10767a3ad7",
+    "url": "/Products/index.html"
+  },
+  {
+    "revision": "2b59d50cad7b25655e7e",
+    "url": "/Products/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2b59d50cad7b25655e7e",
+    "url": "/Products/static/js/2.669625c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Products/static/js/2.669625c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f784b81541eb45dd616f",
+    "url": "/Products/static/js/main.224991be.chunk.js"
+  },
+  {
+    "revision": "7a88d3c07b588bfda9bd",
+    "url": "/Products/static/js/runtime-main.bf866236.js"
+  }
+]);
